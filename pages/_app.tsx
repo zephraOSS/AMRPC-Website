4d57ced56d65d88df39c5338b8fcca0e51eff1cf
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-09-28 17:08:56 +0200

    Use Next.js

---
+++ pages/_app.tsx
@@ -0,0 +1,125 @@
+import type { AppProps } from "next/app";
+
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta name="msapplication-TileColor" content="#fa586a" />
+                <meta
+                    name="msapplication-TileImage"
+                    content="/favicon/ms-icon-144x144.png"
+                />
+                <meta name="theme-color" content="#fa586a" />
+                <meta
+                    name="keywords"
+                    content="Apple Music RPC, iTunes RPC, Discord Apple Music RPC, Discord RPC open-source"
+                />
+                <meta
+                    name="description"
+                    content="AMRPC (Apple Music RPC) is a Discord Rich Presence for Apple Music and iTunes."
+                />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="57x57"
+                    href="/favicon/apple-icon-57x57.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="60x60"
+                    href="/favicon/apple-icon-60x60.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="72x72"
+                    href="/favicon/apple-icon-72x72.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="76x76"
+                    href="/favicon/apple-icon-76x76.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="114x114"
+                    href="/favicon/apple-icon-114x114.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="120x120"
+                    href="/favicon/apple-icon-120x120.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="144x144"
+                    href="/favicon/apple-icon-144x144.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="152x152"
+                    href="/favicon/apple-icon-152x152.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-icon-180x180.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/favicon/android-icon-192x192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href="/favicon/favicon-96x96.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link
+                    rel="shortcut icon"
+                    type="image/x-icon"
+                    href="/favicon/favicon.ico"
+                />
+                <link
+                    rel="icon"
+                    type="image/x-icon"
+                    href="/favicon/favicon.ico"
+                />
+
+                <link rel="manifest" href="/static/manifest.json" />
+                <link
+                    rel="stylesheet"
+                    href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css"
+                />
+
+                <title>AMRPC</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,132 @@
+import type { NextPage } from "next";
+
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.headerNavigation}>
+                    <a href="#download">Download</a>
+                </div>
+                <div className={styles.headerLogo}>
+                    <img
+                        src="https://cdn.zephra.cloud/amrpc/applemusic.png"
+                        alt="AMRPC Logo"
+                    />
+                </div>
+                <div className={styles.headerTitle}>
+                    <h1 className={styles.titleTitle}>AMRPC V4.0</h1>
+                    <h2 className={styles.titleDescription}>
+                        An Open-Source Discord RPC App for Apple Music and
+                        iTunes
+                    </h2>
+                </div>
+            </div>
+
+            <div className={styles.container}>
+                <div className={styles.appExample}>
+                    <Image
+                        src="/App.png"
+                        width={797}
+                        height={598}
+                        alt="Screenshot of the app"
+                    />
+                </div>
+
+                <div className={styles.section}>
+                    <h1 className={styles.sectionTitle}>Features</h1>
+
+                    <div className={styles.featuresList}>
+                        <div className={styles.featuresFeature}>
+                            <div className={styles.featureIcon}>
+                                <i className="fa-solid fa-brush"></i>
+                            </div>
+                            <div className={styles.featureDescription}>
+                                <p>
+                                    The album artwork is displayed in the
+                                    Discord RPC!
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={styles.featuresFeature}>
+                            <div className={styles.featureIcon}>
+                                <i className="fa-solid fa-users"></i>
+                            </div>
+                            <div className={styles.featureDescription}>
+                                <div className={styles.comingSoonContainer}>
+                                    <span className={styles.comingSoon}>
+                                        Coming later this year
+                                    </span>
+                                </div>
+                                <p>
+                                    You can listen together to Apple Music with
+                                    your friends, even if you're not in the same
+                                    place!
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={styles.featuresFeature}>
+                            <div className={styles.featureIcon}>
+                                <i className="fa-solid fa-meteor"></i>
+                            </div>
+                            <div className={styles.featureDescription}>
+                                <p>
+                                    AMRPC is available for Apple Music, iTunes
+                                    and Apple Music Electron on macOS and
+                                    Windows.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.section} id="download">
+                    <div>
+                        <h1 className={styles.sectionTitle}>Download</h1>
+                        <h2 className={styles.sectionDescription}>
+                            AMRPC is currently only available on GitHub.
+                        </h2>
+                    </div>
+
+                    <div className={styles.downloadLinks}>
+                        <a href="https://github.com/ZephraCloud/Apple-Music-RPC/releases/latest">
+                            <img
+                                src="https://cdn.zephra.cloud/amrpc/download_github.png"
+                                alt="GitHub"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <footer className={styles.footer}>
+                <div className={styles.footerNote}>
+                    <span>
+                        Made with 💖 by{" "}
+                        <a href="https://www.zephra.cloud">zephra</a>.
+                    </span>
+                </div>
+                <div className={styles.footerNote} style={{ display: "none" }}>
+                    <span>
+                        AMRPC is not affiliated with Apple or the Apple Music
+                        service.
+                    </span>
+                </div>
+                <div className={styles.footerLinks}>
+                    <a href="https://www.zephra.cloud/legal/imprint">Imprint</a>
+                    <a href="https://github.com/ZephraCloud/Apple-Music-RPC">
+                        GitHub
+                    </a>
+                    <a href="https://twitter.com/zephraCloud">Twitter</a>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Home;
