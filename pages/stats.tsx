190e1eae5d76e0ddfa207c704181ec68dd86595a
Frederik <53608074+N0chteil@users.noreply.github.com>
2023-08-12 14:51:41 +0200

    feat: update stats (8/12/23)

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -27,8 +27,8 @@ export default function Stats() {
             setSelectedVer: React.Dispatch<any>
         ] = React.useState("Latest");
 
-    // as of 7/29/2023
-    const msStoreAcquisitions = 3150,
+    // as of 8/12/2023
+    const msStoreAcquisitions = 3500,
         maxCount = 3000;
 
     const ignoreFiles = ["latest.yml", "latest-mac.yml"];
@@ -176,7 +176,7 @@ export default function Stats() {
                                     opacity: "0.8"
                                 }}
                             >
-                                As of 7/29/2023
+                                As of 8/12/2023
                             </span>
                         </h1>
 
